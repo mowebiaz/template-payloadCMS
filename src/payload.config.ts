1dc54c198262eb78c06897e5d17e3d11e3069ec0
mowebiaz <mowebiaz@gmail.com>
2025-07-12 12:17:46 +0200

    add translation for admin panel

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -4,29 +4,40 @@ import { postgresAdapter } from '@payloadcms/db-postgres'
 import { buildConfig } from 'payload'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { Users } from './collections/Users'
+import { Media } from './collections/Media'
+import { en } from '@payloadcms/translations/languages/en'
+import { fr } from '@payloadcms/translations/languages/fr'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  i18n: {
+    // Internationalization configuration
+    fallbackLanguage: 'en',
+    supportedLanguages: { fr, en },
+  },
+
   // If you'd like to use Rich Text, pass your editor here
   editor: lexicalEditor(),
 
   // Define and configure your collections in this array
-  collections: [],
+  collections: [Users, Media],
 
   // Your Payload secret - should be a complex and secure string, unguessable
   secret: process.env.PAYLOAD_SECRET || '',
-    typescript: {
+  typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
+
   // Whichever Database Adapter you're using should go here
-  // Mongoose is shown as an example, but you can also use Postgres
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
   }),
+  
   // If you want to resize images, crop, set focal point, etc.
   // make sure to install it and pass it to the config.
   // This is optional - if you don't need to do these things,
