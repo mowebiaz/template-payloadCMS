f3bae288bfecc7a131068f490c7cecd316655b57
mowebiaz <mowebiaz@gmail.com>
2025-07-14 09:59:03 +0200

    translate date format of admin panel

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -13,6 +13,9 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  admin: {
+    dateFormat: 'dd/MM/yyyy',
+  },
   i18n: {
     // Internationalization configuration
     fallbackLanguage: 'en',
@@ -37,7 +40,7 @@ export default buildConfig({
       connectionString: process.env.DATABASE_URI || '',
     },
   }),
-  
+
   // If you want to resize images, crop, set focal point, etc.
   // make sure to install it and pass it to the config.
   // This is optional - if you don't need to do these things,
