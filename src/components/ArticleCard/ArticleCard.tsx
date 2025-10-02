c7a64a6e4c7c288ccaef5a8b8d53e670e3269919
mowebiaz <mowebiaz@gmail.com>
2025-10-02 11:45:16 +0200

    Modify ArticleCard logic

--- src/components/ArticleCard/ArticleCard.tsx
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -29,8 +29,7 @@ export const ArticleCard: React.FC<{
   const hasCategories = categories && Array.isArray(categories) && categories.length > 0
   const titleToUse = titleFromProps || title
   const sanitizedDescription = description?.replace(/\s/g, ' ') // replace non-breaking space with white space
-  //const href = `/${relationTo}/${slug}`
-  const href = `/posts/${slug}`
+  const href = `/${relationTo}/${slug}`
 
   return (
     <article
