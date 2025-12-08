8fcba6a700502321c7138875e458217a3afcaff7
mowebiaz <mowebiaz@gmail.com>
2025-12-08 17:02:04 +0100

    modify migration

--- src/migrations/20251208_132241.ts
+++ src/migrations/20251208_132241.ts
@@ -1,15 +1,42 @@
-import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-vercel-postgres'
+import {
+  MigrateDownArgs,
+  MigrateUpArgs,
+  sql,
+} from '@payloadcms/db-vercel-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "categories" ADD COLUMN "slug" varchar NOT NULL;
-  ALTER TABLE "categories" ADD COLUMN "slug_lock" boolean DEFAULT true;
-  CREATE INDEX "categories_slug_idx" ON "categories" USING btree ("slug");`)
+   ALTER TABLE "categories" ADD COLUMN "slug" varchar;`)
+
+  await db.execute(sql`
+    ALTER TABLE "categories"
+    ADD COLUMN "slug_lock" boolean DEFAULT true;`)
+
+  await db.execute(sql`
+    UPDATE "categories"
+    SET "slug" = 'slugpardefaut'
+    WHERE "slug" IS NULL;`)
+
+  await db.execute(sql`
+    ALTER TABLE "categories"
+    ALTER COLUMN "slug" SET NOT NULL;`)
+
+  await db.execute(sql`
+CREATE INDEX IF NOT EXISTS "categories_slug_idx" ON "categories" USING btree ("slug");`)
 }
 
-export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+export async function down({
+  db,
+  payload,
+  req,
+}: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
-   DROP INDEX "categories_slug_idx";
-  ALTER TABLE "categories" DROP COLUMN "slug";
+   DROP INDEX IF EXISTS "categories_slug_idx";`)
+
+  await db.execute(sql`
+
+  ALTER TABLE "categories" DROP COLUMN "slug";`)
+
+  await db.execute(sql`
   ALTER TABLE "categories" DROP COLUMN "slug_lock";`)
 }
